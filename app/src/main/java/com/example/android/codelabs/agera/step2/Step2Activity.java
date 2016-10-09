ba11829000bf820a2c34861cfb3e5c9b071a0e78
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-10-09 17:02:05 +0900

    update complex repositories

--- app/src/main/java/com/example/android/codelabs/agera/step2/Step2Activity.java
+++ app/src/main/java/com/example/android/codelabs/agera/step2/Step2Activity.java
@@ -18,6 +18,7 @@ package com.example.android.codelabs.agera.step2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,6 +28,8 @@ import com.google.android.agera.Repositories;
 import com.google.android.agera.Repository;
 import com.google.android.agera.Updatable;
 
+import java.util.Locale;
+
 public class Step2Activity extends AppCompatActivity {
 
     public static final String VALUE_KEY = "VALUE_KEY";
@@ -35,37 +38,46 @@ public class Step2Activity extends AppCompatActivity {
 
     private Repository<String> textValueRepository;
 
-    private Updatable mTextValueUpdatable;
+    private Updatable textValueUpdatable;
 
-    private TextView mValueTv;
+    private TextView valueTv;
 
-    private Button mIncrementBt;
+    private Button incrementBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.step2);
 
-        mValueTv = (TextView) findViewById(R.id.step2_value_tv);
-        mIncrementBt = (Button) findViewById(R.id.increment_bt);
+        valueTv = (TextView) findViewById(R.id.step2_value_tv);
+        incrementBt = (Button) findViewById(R.id.increment_bt);
 
         // Set onClickListener
+        incrementBt.setOnClickListener(view -> valueRepository.accept(valueRepository.get() + 1));
 
         // Create complex repository:
-        // textValueRepository = Repositories.repositoryWithInitialValue("N/A")
+        textValueRepository = Repositories.repositoryWithInitialValue("N/A")
+                .observe(valueRepository)
+                .onUpdatesPerLoop()
+                .getFrom(valueRepository)
+                .thenTransform(input -> String.format(Locale.getDefault(), "%d", input))
+                .compile();
 
         // Create updatable
+        textValueUpdatable = () -> valueTv.setText(textValueRepository.get());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // Add updatables here.
+        textValueRepository.addUpdatable(textValueUpdatable);
     }
 
     @Override
     protected void onStop() {
         // Remove updatables here.
+        textValueRepository.removeUpdatable(textValueUpdatable);
         super.onStop();
     }
 }
